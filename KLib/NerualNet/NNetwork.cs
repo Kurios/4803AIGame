a5561fc251adab100f671bb8a75fe23691c0e95a
kurios <jack.morgan@kuriosly.com>
2012-10-15 00:19:26 -0400

    Added Neural Network Nodes.

---
+++ KLib/NerualNet/NNetwork.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace KLib.NerualNet
+{
+    public class NNetwork
+    {
+
+        public LinkedList<Node> Nodes = new LinkedList<Node>();
+        public LinkedList<Node> Outputs = new LinkedList<Node>();
+
+        int lastTick = 0;
+
+        public NNetwork()
+        {
+        }
+
+        public void update()
+        {
+            foreach (Node node in Outputs)
+            {
+                node.update(lastTick);
+                lastTick++;
+            }
+        }
+    }
+}
