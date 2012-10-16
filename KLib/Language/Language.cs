b89286dabf2df9c739413c1468e4de7da0a00284
kurios <jack.morgan@kuriosly.com>
2012-10-16 16:57:46 -0400

    Our Current Ponderings As on the whiteboard and such....

--- KLib/Language/Language.cs
+++ KLib/Language/Language.cs
@@ -6,6 +6,21 @@ using KLib.NerualNet.emotionState;
 
 namespace KLib.Language
 {
+
+    /**
+     * More random thoughts!
+     * 
+     * Actually, from our talk.
+     * 
+     * What is the meaning of a word, and well... 
+     * 
+     * it has an emoitonal context and meaning... and the combination of words change the meaning of words... 
+     * but yah... welcome to a fun problem of trying to make something that is implicit to humans understandable to computers
+     * 
+     * But the word itself is meaningless. 
+     * 
+     *  -Kurios 10/16/2012
+     */
     public class Language
     {
         eSpace eSpace = new eSpace();
